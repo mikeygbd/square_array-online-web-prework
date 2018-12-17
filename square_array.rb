9429e35ba3b2cd64425f3a6b65bdb3040a308939
Michael Soares <mikey.soares@gmail.com>
2018-12-17 15:24:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,19 +1,9 @@
 def square_array(array)
   # your code here
   new_num = []
-  array.each do |num|
+  array.collect do |num|
   new_num << num * num
 
 end
 new_num
-end
-
-def square_array(array)
-  # your code here
-  
-  array.collect do |num|
-   num * num
-
-end
-array
 end
